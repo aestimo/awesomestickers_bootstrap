92db8cec97410685b46e1f7ac7c8d95af184136a
Aestimo Njuguna <lee.njuguna@gmail.com>
2014-07-08 10:19:26 +0300

    backing up the capistrano files...

--- older-capistrano-backup/deploy.rb
+++ older-capistrano-backup/deploy.rb
@@ -1,26 +1,26 @@
 # config valid only for Capistrano 3.1
-lock '3.1.0'
+lock '3.2.1'
 
-set :application, 'foobar'                       # application name
-set :repo_url, 'git@github.com:aestimo/awesomestickers_bootstrap.git'   # your repo url
-set :deploy_to, '/home/deploy/apps/awesomestickers_bootstrap'
-set :password, ask('Server password', nil)
-server 'server.domain.com', user: 'deploy', port: 22, password: fetch(:password), roles: %w{web app db}
+set :application, 'awesomestickers_bootstrap'
+set :repo_url, 'git@github.com:aestimo/awesomestickers_bootstrap.git'
+
+set :deploy_to, '/home/deploy/awesomestickers_bootstrap'
+
+set :linked_files, %w{config/database.yml}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :pty, false
+
 # Default branch is :master
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 
 # Default deploy_to directory is /var/www/my_app
 # set :deploy_to, '/var/www/my_app'
 
 # Default value for :scm is :git
-set :scm, :git
-set :branch, 'master'
-set :keep_releases, 5
+# set :scm, :git
 
 # Default value for :format is :pretty
-set :format, :pretty
-set :log_level, :debug
-set :pty, true
+# set :format, :pretty
 
 # Default value for :log_level is :debug
 # set :log_level, :debug
@@ -30,29 +30,15 @@ set :pty, true
 
 # Default value for :linked_files is []
 # set :linked_files, %w{config/database.yml}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+# Default value for linked_dirs is []
+# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
 # Default value for keep_releases is 5
-# set :keep_releases, 5
-
-# configs to set Puma
-
-set :puma_rackup, -> { File.join(current_path, 'config.ru') }
-set :puma_state, "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
-set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"
-set :puma_conf, "#{shared_path}/puma.rb"
-set :puma_access_log, "#{shared_path}/log/puma_error.log"
-set :puma_error_log, "#{shared_path}/log/puma_access.log"
-set :puma_role, :app
-set :puma_env, fetch(:rack_env, fetch(:rails_env, 'production'))
-set :puma_threads, [0, 16]
-set :puma_workers, 0
-set :puma_init_active_record, true
-set :puma_preload_app, true
+set :keep_releases, 5
 
 namespace :deploy do
 
