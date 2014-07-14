95588e57fff456acd244c142a34edc4621c4231b
Aestimo Njuguna <lee.njuguna@gmail.com>
2014-07-14 12:45:08 +0300

    hopefully a fix for the rvm issue

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,1 +1,3 @@
 set :deploy_to, "/home/deploy/rails_projects/awesomestickers_staging"
+
+# TODO: got a failed: "rvm_path=$HOME/.rvm $HOME/.rvm/bin/rvm-shell 'default' -c when running cap deploy :cold -8/7/14
